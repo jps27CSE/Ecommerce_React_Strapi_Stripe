becfd1a43056baa03fc496fee06bb681a15d2884
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2023-05-18 00:12:56 +0600

    Strip Api Test

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -2,7 +2,20 @@ import "./Home.scss";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
+import { fetchDataFromApi } from "../../utils/api";
+import { useEffect } from "react";
+
 const Home = () => {
+  useEffect(() => {
+    getCategoris();
+  }, []);
+
+  const getCategoris = () => {
+    fetchDataFromApi("/api/categories?populate=*").then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <div>
       <Banner />
