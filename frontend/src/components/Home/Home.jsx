7d145d9decbab25763352773a3297c50f82261f8
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2023-05-18 15:13:43 +0600

    API setup for Homepage, Categories and Products sections

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -3,16 +3,29 @@ import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
 import { fetchDataFromApi } from "../../utils/api";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import { Context } from "../../utils/context";
 
 const Home = () => {
+  const { categories, setCategories, products, setProducts } =
+    useContext(Context);
+
   useEffect(() => {
+    getProducts();
     getCategoris();
   }, []);
+
+  const getProducts = () => {
+    fetchDataFromApi("/api/products?populate=*").then((response) => {
+      console.log(response);
+      setProducts(response);
+    });
+  };
 
   const getCategoris = () => {
     fetchDataFromApi("/api/categories?populate=*").then((response) => {
       console.log(response);
+      setCategories(response);
     });
   };
 
@@ -21,8 +34,8 @@ const Home = () => {
       <Banner />
       <div className="main-content">
         <div className="layout">
-          <Category />
-          <Products headingText="Popular Products" />
+          <Category categories={categories} />
+          <Products products={products} headingText="Popular Products" />
         </div>
       </div>
     </div>
