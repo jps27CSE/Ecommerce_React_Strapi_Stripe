c7949cfa4321545ebcfcda4dfe3ffdadc23e3369
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2023-05-19 12:57:32 +0600

    Product Details page Api integration

--- frontend/src/components/Products/Product/Product.jsx
+++ frontend/src/components/Products/Product/Product.jsx
@@ -1,8 +1,10 @@
 import "./Product.scss";
-import prod from "../../../assets/products/earbuds-prod-1.webp";
+import { useNavigate } from "react-router-dom";
+
 const Product = ({ id, data }) => {
+  const navigate = useNavigate();
   return (
-    <div className="product-card">
+    <div className="product-card" onClick={() => navigate("/product/" + id)}>
       <div className="thumbnail">
         <img
           src={
--- frontend/src/components/SingleProduct/SingleProduct.jsx
+++ frontend/src/components/SingleProduct/SingleProduct.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { Context } from "../../utils/context";
 import { useParams } from "react-router-dom";
-// import useFetch from "../../hooks/useFetch";
+import useFetch from "../../hooks/useFetch";
 import RelatedProducts from "./RelatedProducts/RelatedProducts";
 import {
   FaFacebookF,
@@ -15,22 +15,46 @@ import "./SingleProduct.scss";
 import prod from "../../assets/products/earbuds-prod-1.webp";
 
 const SingleProduct = () => {
+  const [quantity, setQuantity] = useState(1);
+  const { id } = useParams();
+  const { data } = useFetch(`/api/products?populate=*&[filters][id]=${id}`);
+
+  const decrement = () => {
+    setQuantity((prevState) => {
+      if (prevState === 1) return 1;
+      return prevState - 1;
+    });
+  };
+
+  const increment = () => {
+    setQuantity((prevState) => prevState + 1);
+  };
+
+  if (!data) return;
+  const product = data?.data?.[0]?.attributes;
+  console.log(product);
   return (
     <div className="single-product-main-content">
       <div className="layout">
         <div className="single-product-page">
           <div className="left">
-            <img src={prod} alt="" />
+            <img
+              src={
+                process.env.REACT_APP_STRIPE_APP_DEV_URL +
+                product.img.data[0].attributes.url
+              }
+              alt=""
+            />
           </div>
           <div className="right">
-            <span className="name">Product Name</span>
-            <span className="price">Price</span>
-            <span className="desc">Product Description</span>
+            <span className="name">{product.title}</span>
+            <span className="price">&#8377;{product.price}</span>
+            <span className="desc">{product.description}</span>
             <div className="cart-buttons">
               <div className="quantity-buttons">
-                <span>-</span>
-                <span>5</span>
-                <span>+</span>
+                <span onClick={decrement}>-</span>
+                <span>{quantity}</span>
+                <span onClick={increment}>+</span>
               </div>
               <button className="add-to-cart-button">
                 <FaCartPlus size={20} />
@@ -40,7 +64,8 @@ const SingleProduct = () => {
             <span className="divider" />
             <div className="info-item">
               <span className="text-bold">
-                Category: <span>Headphones</span>
+                Category:{" "}
+                <span> {product.categories.data[0].attributes.title}</span>
               </span>
               <span className="text-bold">
                 Share:
